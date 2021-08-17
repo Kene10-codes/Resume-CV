6cf862bd5a21f8a973a2eb342e5ee4f8815f4f5e
keneNwobodo <nwobodokenechukwu2@gmail.com>
2021-08-17 16:16:59 +0100

    fix some updates

---
+++ script.js
@@ -0,0 +1,11 @@
+console.clear();
+
+// print full name
+let fullName;
+fullName = "Nwobodo Kenechukwu";
+//convert name to uppercase 
+let fullNameUpperCase = fullName.toUpperCase();
+console.log(fullNameUpperCase);
+
+
+
